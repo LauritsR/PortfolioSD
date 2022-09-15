880e55927c875f6589391b2ec6095bcbfab34ac9
LauritsR <94968975+LauritsR@users.noreply.github.com>
2022-09-15 11:44:12 +0200

    Created test for loading zero liquid

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Tanker tanker = new Tanker("Denmark", 10, 10, 10, 10);
-        tanker.loadingCargo(0, 0.7);
+        tanker.loadingCargo(0, 0.2);
 
     }
 }
---
+++ src/com/company/TankerTest.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import org.testng.annotations.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TankerTest {
+
+  @Test
+  void throwsExeptionWhenZero(){
+      Tanker tanker = new Tanker("Denmark", 10, 10, 10, 10);
+      assertThrows(IllegalArgumentException.class,
+          () -> {
+                tanker.loadingCargo(0, 0);
+          });
+  }
+}
